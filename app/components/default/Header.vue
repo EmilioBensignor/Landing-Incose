00e1aa6112b9460f290d4a6eeead22fd2a288cf2
Emilio Bensignor <liobensignor@gmail.com>
2025-09-08 01:21:43 -0300

    Logo a Home + Posts

--- app/components/default/Header.vue
+++ app/components/default/Header.vue
@@ -1,8 +1,10 @@
 <template>
     <header class="sticky top-0 z-50 bg-dark/70 shadow-md py-4 lg:py-8 px-5 md:px-12 lg:px-16 xxl:px-20">
         <div class="max-w-[1280px] flex justify-between items-center   mx-auto">
-            <NuxtImg src="/images/Logo-Incose.svg" alt="Logo Incose"
-                class="w-12 lg:w-20 h-11 lg:h-[4.75rem] object-contain" />
+            <NuxtLink :to="ROUTE_NAMES.HOME">
+                <NuxtImg src="/images/Logo-Incose.svg" alt="Logo Incose"
+                    class="w-12 lg:w-20 h-11 lg:h-[4.75rem] object-contain" />
+            </NuxtLink>
             <nav>
                 <ul class="flex items-center gap-3">
                     <li v-for="(red, index) in redes" :key="index">
@@ -18,5 +20,6 @@
 </template>
 
 <script setup>
+import { ROUTE_NAMES } from '~/constants/ROUTE_NAMES';
 import redes from '~/shared/redes';
 </script>
--- app/shared/posts.js
+++ app/shared/posts.js
@@ -35,6 +35,42 @@ const posts = [
             "Vulputate lacus platea scelerisque morbi hac justo molestie massa, rhoncus dictum consequat cum blandit eros nostra euismod interdum, eu ante turpis orci gravida aptent pulvinar. Eu pharetra curabitur diam risus turpis eleifend urna hendrerit montes tempor, magnis vestibulum sapien duis vehicula platea vitae nibh leo nam, ultrices dictumst suspendisse lectus sociis quis quisque est imperdiet. Tempor ut molestie porta natoque sollicitudin augue, dignissim nisl auctor congue pulvinar, odio scelerisque cum mi magnis."
         ],
     },
+    {
+        slug: "entendiendo-la-resolucion-236-2024",
+        img: '/images/Placeholder.png',
+        categoria: 'Actualizaciones',
+        titulo: 'Entendiendo la resolución 236/2024',
+        subtitulo: 'Explora los puntos clave del nuevo reglamento',
+        textos: [
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit a bibendum quisque, nibh eget curae porttitor ridiculus fermentum cubilia sodales imperdiet iaculis vitae, fames mi litora orci pretium nunc senectus penatibus duis. Habitant ac ligula risus quis netus egestas montes vestibulum donec quisque metus suspendisse, tortor duis consequat lacus augue primis fermentum ad arcu pulvinar cubilia. Suspendisse massa mus interdum vulputate ad nascetur egestas class blandit diam, sed venenatis nam dictumst odio mollis id dis commodo penatibus, duis nec varius purus hendrerit vestibulum ultricies fusce est. Elementum bibendum eleifend quam hendrerit inceptos massa, volutpat a vulputate fermentum conubia lectus, tortor sagittis sodales semper suspendisse, sem iaculis class proin pharetra.",
+            "Quis felis curae vulputate nisl nec orci fusce tortor, nisi tempor magna vel vitae sodales parturient congue nulla, taciti blandit risus euismod morbi eleifend pulvinar. Senectus eu parturient sem sodales ac sapien dignissim quam sociosqu curabitur posuere, turpis justo ad velit vulputate dictumst laoreet elementum mauris lacinia. Hendrerit diam hac phasellus enim aliquam elementum aptent ante dui, justo senectus curabitur nisi gravida ligula integer imperdiet, maecenas cursus conubia blandit tellus sociosqu felis himenaeos. Mattis volutpat sagittis condimentum nunc tellus justo vulputate, netus lobortis viverra arcu mus nostra leo per, sed ridiculus convallis cursus risus mauris.",
+            "Vulputate lacus platea scelerisque morbi hac justo molestie massa, rhoncus dictum consequat cum blandit eros nostra euismod interdum, eu ante turpis orci gravida aptent pulvinar. Eu pharetra curabitur diam risus turpis eleifend urna hendrerit montes tempor, magnis vestibulum sapien duis vehicula platea vitae nibh leo nam, ultrices dictumst suspendisse lectus sociis quis quisque est imperdiet. Tempor ut molestie porta natoque sollicitudin augue, dignissim nisl auctor congue pulvinar, odio scelerisque cum mi magnis."
+        ],
+    },
+    {
+        slug: "la-importancia-de-la-certificacion",
+        img: '/images/Placeholder.png',
+        categoria: 'Perspectivas',
+        titulo: 'La importancia de la certificación',
+        subtitulo: 'Porque son importantes los perfiles certificados de la construcción en seco',
+        textos: [
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit a bibendum quisque, nibh eget curae porttitor ridiculus fermentum cubilia sodales imperdiet iaculis vitae, fames mi litora orci pretium nunc senectus penatibus duis. Habitant ac ligula risus quis netus egestas montes vestibulum donec quisque metus suspendisse, tortor duis consequat lacus augue primis fermentum ad arcu pulvinar cubilia. Suspendisse massa mus interdum vulputate ad nascetur egestas class blandit diam, sed venenatis nam dictumst odio mollis id dis commodo penatibus, duis nec varius purus hendrerit vestibulum ultricies fusce est. Elementum bibendum eleifend quam hendrerit inceptos massa, volutpat a vulputate fermentum conubia lectus, tortor sagittis sodales semper suspendisse, sem iaculis class proin pharetra.",
+            "Quis felis curae vulputate nisl nec orci fusce tortor, nisi tempor magna vel vitae sodales parturient congue nulla, taciti blandit risus euismod morbi eleifend pulvinar. Senectus eu parturient sem sodales ac sapien dignissim quam sociosqu curabitur posuere, turpis justo ad velit vulputate dictumst laoreet elementum mauris lacinia. Hendrerit diam hac phasellus enim aliquam elementum aptent ante dui, justo senectus curabitur nisi gravida ligula integer imperdiet, maecenas cursus conubia blandit tellus sociosqu felis himenaeos. Mattis volutpat sagittis condimentum nunc tellus justo vulputate, netus lobortis viverra arcu mus nostra leo per, sed ridiculus convallis cursus risus mauris.",
+            "Vulputate lacus platea scelerisque morbi hac justo molestie massa, rhoncus dictum consequat cum blandit eros nostra euismod interdum, eu ante turpis orci gravida aptent pulvinar. Eu pharetra curabitur diam risus turpis eleifend urna hendrerit montes tempor, magnis vestibulum sapien duis vehicula platea vitae nibh leo nam, ultrices dictumst suspendisse lectus sociis quis quisque est imperdiet. Tempor ut molestie porta natoque sollicitudin augue, dignissim nisl auctor congue pulvinar, odio scelerisque cum mi magnis."
+        ],
+    },
+    {
+        slug: "navegando-por-las-nuevas-resoluciones",
+        img: '/images/Placeholder.png',
+        categoria: 'Actualizaciones',
+        titulo: 'Navegando por las nuevas resoluciones',
+        subtitulo: 'Una guía para comprender los cambios recientes en las regulaciones',
+        textos: [
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit a bibendum quisque, nibh eget curae porttitor ridiculus fermentum cubilia sodales imperdiet iaculis vitae, fames mi litora orci pretium nunc senectus penatibus duis. Habitant ac ligula risus quis netus egestas montes vestibulum donec quisque metus suspendisse, tortor duis consequat lacus augue primis fermentum ad arcu pulvinar cubilia. Suspendisse massa mus interdum vulputate ad nascetur egestas class blandit diam, sed venenatis nam dictumst odio mollis id dis commodo penatibus, duis nec varius purus hendrerit vestibulum ultricies fusce est. Elementum bibendum eleifend quam hendrerit inceptos massa, volutpat a vulputate fermentum conubia lectus, tortor sagittis sodales semper suspendisse, sem iaculis class proin pharetra.",
+            "Quis felis curae vulputate nisl nec orci fusce tortor, nisi tempor magna vel vitae sodales parturient congue nulla, taciti blandit risus euismod morbi eleifend pulvinar. Senectus eu parturient sem sodales ac sapien dignissim quam sociosqu curabitur posuere, turpis justo ad velit vulputate dictumst laoreet elementum mauris lacinia. Hendrerit diam hac phasellus enim aliquam elementum aptent ante dui, justo senectus curabitur nisi gravida ligula integer imperdiet, maecenas cursus conubia blandit tellus sociosqu felis himenaeos. Mattis volutpat sagittis condimentum nunc tellus justo vulputate, netus lobortis viverra arcu mus nostra leo per, sed ridiculus convallis cursus risus mauris.",
+            "Vulputate lacus platea scelerisque morbi hac justo molestie massa, rhoncus dictum consequat cum blandit eros nostra euismod interdum, eu ante turpis orci gravida aptent pulvinar. Eu pharetra curabitur diam risus turpis eleifend urna hendrerit montes tempor, magnis vestibulum sapien duis vehicula platea vitae nibh leo nam, ultrices dictumst suspendisse lectus sociis quis quisque est imperdiet. Tempor ut molestie porta natoque sollicitudin augue, dignissim nisl auctor congue pulvinar, odio scelerisque cum mi magnis."
+        ],
+    },
 ];
 
 export default posts;
