1cd149ad22e7bdc93222b934bb05c9f0d9817511
Lara <laracrupni@gmail.com>
2025-09-16 17:33:29 -0300

    Cambio por Logo Blanco

--- app/components/default/Footer.vue
+++ app/components/default/Footer.vue
@@ -2,7 +2,7 @@
     <footer id="footer" class="bg-dark text-white py-16 lg:py-20 px-5 md:px-12 lg:px-16">
         <div class="w-full max-w-[1280px] flex flex-col items-center gap-6 lg:gap-20 mx-auto">
             <div class="w-full flex flex-col justify-between lg:flex-row items-center gap-16 lg:gap-32">
-                <NuxtImg src="/images/Logo-Incose.svg" alt="Logo Incose" class="w-20 h-[4.75rem] object-contain" />
+                <NuxtImg src="/images/Logo-Incose-Blanco.svg" alt="Logo Incose" class="w-20 h-[4.75rem] object-contain" />
                 <div class="flex flex-col gap-6">
                     <ul class="flex flex-col lg:flex-row items-center gap-6">
                         <li>
