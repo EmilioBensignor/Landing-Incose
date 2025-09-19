c486de71b8192af3ca66a588bb5598e9d180a90b
Lara <laracrupni@gmail.com>
2025-09-19 12:25:37 -0300

    Modificaciones Posts Pedido Cliente

--- app/shared/posts.js
+++ app/shared/posts.js
@@ -7,11 +7,11 @@ const posts = [
         subtitulo: 'Construcción en seco y el Steel Framing.- Cuidá tu obra y tu reputación',
         textos: [
             "La industria de la construcción en Argentina está en constante cambio. Y actualmente estamos experimentando uno de los más grandes e importantes de los últimos años.",
-            "Frente a las nuevas flexibilizaciones de las importaciones, INCOSE impulsa la Resolución 236, que establece un nuevo Reglamento Técnico de Productos para la Construcción.",
+            "El INCOSE promueve el cumplimiento de la Resolución 236/2024, que establece un nuevo Reglamento Técnico de Productos para la Construcción.",
             "¡Y tenés que saber todo lo que implica… y cómo puede afectar a tus proyectos!",
-            "Esta normativa es un hito para la industria. A partir de ahora, los perfiles de acero para construcción en seco y Steel Framing, que son fundamentales para la seguridad y durabilidad de tus proyectos, deberán estar certificados bajo normas IRAM. Esto significa un mayor control y una garantía de calidad para todos.",
+            "Esta normativa es un hito para la industria ya que reafirma que los perfiles de acero para construcción en seco y Steel Framing, que son fundamentales para la seguridad y durabilidad de tus proyectos, deben estar certificados bajo normas IRAM. Esto significa un mayor control y una garantía de calidad para todos.",
             "El objetivo es claro: proteger la inversión de los clientes, asegurar la estabilidad de las construcciones y elevar los estándares de la industria.",
-            "Desde INCOSE, te acompañamos en esta transición para que puedas seguir construyendo con la máxima confianza, y te invitamos a conocer todos los detalles de esta resolución y a prepararte para este cambio. Todos los detalles ya están disponibles en nuestra web para que puedas consultarlos."
+            "Desde INCOSE, te acompañamos en este cambio para que puedas seguir construyendo con la máxima confianza, y te invitamos a conocer todos los detalles de esta Resolución."
         ],
     },
     {
@@ -21,13 +21,12 @@ const posts = [
         titulo: 'Los peligros de los perfiles NO certificados',
         subtitulo: 'Conocé los riesgos y evitá complicaciones en tu obra. La seguridad no se negocia.',
         textos: [
-            "Como profesionales de la construcción, sabemos que la elección de materiales es una de las decisiones más importantes de una obra de construcción en seco o Steel Framing. Sin embargo, no todos los perfiles de acero en el mercado garantizan la misma calidad. El uso de perfiles no certificados, aunque pueda parecer una opción más accesible a corto plazo, conlleva riesgos significativos que pueden comprometer todo tu proyecto.",
+            "Como profesionales de la construcción, sabemos que la elección de materiales es una de las decisiones más importantes de una obra de construcción en seco o Steel Framing. Sin embargo, no todos los perfiles de acero presentes en el mercado garantizan la misma calidad. El uso de perfiles no certificados, aunque pueda parecer una opción más accesible a corto plazo, conlleva riesgos significativos que pueden comprometer todo tu proyecto.",
             "La nueva Resolución 236/2024 exige que los perfiles utilizados en la construcción en seco y Steel Framing estén certificados, lo que refuerza la necesidad de una elección consciente. No arriesgues la seguridad de tu obra ni la tranquilidad de tus clientes.",
             "Principales peligros de los perfiles NO certificados:",
             "Fallas Estructurales: La falta de control en los procesos de fabricación y la ausencia de estándares de calidad pueden llevar a que el material no resista las cargas de peso, viento o sismos, comprometiendo la seguridad de la construcción.",
             "Riesgos Legales: Al no cumplir con las normas técnicas obligatorias (como las IRAM), el constructor o desarrollador se expone a infracciones legales.",
-            "Menor Durabilidad: El uso de perfiles no certificados puede implicar una falta en el recubrimiento de zinc, generando corrosión y perforaciones, lo que representa un auténtico riesgo de deformaciones y un eventual colapso.",
-            "Todos los detalles sobre esta normativa ya están disponibles en nuestra web para que puedas consultarlos y garantizar la calidad de tus próximos proyectos."
+            "Menor Durabilidad: El uso de perfiles no certificados puede implicar un menor recubrimiento de zinc, generando corrosión y perforaciones, lo que representa un auténtico riesgo de deformaciones y un eventual colapso."
         ],
     },
     {
@@ -37,22 +36,21 @@ const posts = [
         titulo: 'Lo que tenés que saber sobre los perfiles certificados.',
         subtitulo: 'CALIDAD Y CUMPLIMIENTO: Invertí en la seguridad, durabilidad y valor de tus proyectos.',
         textos: [
-            "Con la entrada en vigencia de la nueva Resolución 236/2024, es fundamental saber identificar los perfiles de acero galvanizado para construcción en seco y Steel Framing, para garantizar así la excelencia y el cumplimiento normativo en sus obras. No solo son un requisito legal, son la base de una construcción segura y duradera.",
-            "¿Cómo identificar un perfil certificado? ¡Es muy sencillo! Los perfiles certificados cuentan con un grabado en sus alas que incluye:",
+            "Con la entrada en vigencia de la nueva Resolución 236/2024, es fundamental saber identificar los perfiles de acero galvanizado para construcción en seco y Steel Framing, para garantizar así la excelencia y el cumplimiento normativo en tus obras. No solo son un requisito legal: son la base de una construcción segura y duradera.",
+            "¿Cómo identificar un perfil certificado? ¡Es muy sencillo! Los perfiles para Steel Framing certificados cuentan con un grabado en sus alas que incluye:",
             "- Nombre del fabricante",
-            "- Espesor de la chapa",
-            "- Tipo de recubrimiento galvanizado",
-            "- Grado estructural del acero (ZAR)",
-            "- Certificación IRAM-IAS U 500-243 (construcción en seco)",
-            "- Certificación IRAM-IAS U 500-205 (Steel Framing)",
-            "- Número de certificación del laboratorio (IRAM o INTI)",
+            "- Designación del perfil con el espesor de la chapa base en mm",
+            "- Tipo de recubrimiento galvanizado Z275",
+            "- Grado estructural del acero (ZAR) en MPa",
+            "- Código de trazabilidad",
+            "- Número de la norma (IRAM IAS U 500-205)",
+            "- Número del certificado (Optativo)",
             "Además, próximamente este proceso será aún más fácil con un sistema de verificación por código QR.",
             "LA IMPORTANCIA DE USAR PERFILES CERTIFICADOS",
             "Calidad y Durabilidad Aseguradas: La certificación garantiza que el perfil cumple con las especificaciones técnicas requeridas, asegurando la resistencia mecánica y la durabilidad de la estructura.",
             "Conformidad Legal y tranquilidad: Además de brindar transparencia, te protege legalmente, indicando que tu proyecto está alineado con las regulaciones vigentes, evitando cualquier riesgo legal o sanción.",
             "Mayor Seguridad: Al usar materiales que cumplen con estrictos controles de calidad, protegés la estabilidad de la construcción y la seguridad de las personas.",
-            "La certificación es tu mejor herramienta para asegurar tu obra… y también tu reputación profesional.",
-            "Todos los contenidos sobre esta normativa ya están disponibles en nuestra web para que puedas consultarlos.",
+            "La certificación es tu mejor herramienta para asegurar tu obra… y también tu reputación profesional."
         ],
     },
     {
@@ -63,8 +61,8 @@ const posts = [
         subtitulo: 'Resolvé tus dudas sobre la nueva normativa.',
         textos: [
             "Sabemos que la implementación de una nueva normativa puede generar dudas. En INCOSE, estamos para brindarte respuestas claras y precisas sobre la Resolución 236 y el nuevo Reglamento Técnico para productos de construcción en Argentina.",
-            "1. ¿Qué productos están alcanzados por esta nueva normativa? Los perfiles de acero galvanizado para construcción en seco y Steel Framing están incluidos en el Reglamento Técnico de la Resolución 236/2024.",
-            "2. ¿Cómo puedo verificar si un perfil está certificado? Buscá el grabado en el ala del perfil que debe incluir el nombre del fabricante y el número del ente certificador IRAM o TÜV. Próximamente, los paquetes de perfiles también llevarán una etiqueta con un código QR que podrás escanear para acceder al certificado y la declaración jurada del fabricante.",
+            "1. ¿Qué productos están alcanzados por esta nueva normativa? Los perfiles de acero galvanizado para construcción en seco y Steel Framing están incluidos en el Reglamento Técnico de la Resolución 236/2024. También los perfiles para cielorrasos suspendidos se encuentran incluidos en el Reglamento.",
+            "2. ¿Cómo puedo verificar si un perfil está certificado? Buscá el grabado en el ala del perfil que debe incluir, en el caso de perfiles para Steel Framing: el nombre del fabricante, la designación del perfil incluyendo el espesor de la chapa base, el recubrimiento galvanizado Z275, la mención de la Norma IRAM IAS U 500-205, un código de trazabilidad y optativamente el número de certificado. En el caso de perfiles para construcción en seco el rotulado debe indicar: el nombre del fabricante, la designación del perfil, el espesor de la chapa base en mm, el código de trazabilidad y la mención de la norma IRAM IAS U 500-243 con la parte de la misma que corresponda. El número de certificado es optativo. Próximamente, los paquetes de perfiles también llevarán una etiqueta con un código QR que podrás escanear para acceder al certificado y la declaración jurada del fabricante.",
             "3. ¿Qué pasa si uso perfiles no certificados? Además de ser una infracción a las reglamentaciones, compromete la seguridad estructural y la durabilidad de la obra. Expone al proyecto a riesgos de fallas y a los profesionales a sanciones legales por incumplimiento.",
             "4. ¿Un informe de laboratorio de calidad es lo mismo que una certificación? No. Un informe de laboratorio se realiza sobre una muestra puntual y no garantiza que toda la producción cumpla con la norma. La certificación, en cambio, implica un sistema de auditorías y trazabilidad que asegura la calidad de toda la producción.",
             "¿Tenés más dudas? Todos los contenidos ya están disponibles en nuestra web para que puedas consultarlos."
